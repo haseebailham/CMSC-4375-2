a02f62ba49c607504b9557ac3894ed79d5010494
b2333981 <b2333981@ben.edu>
2019-10-26 16:50:21 -0500

    added code for environment, app module classes for firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDWasGK3x4njYqasOXTGiE4UZInKQFp9z4",
+    authDomain: "cmsc-4375-lab-3.firebaseapp.com",
+    databaseURL: "https://cmsc-4375-lab-3.firebaseio.com",
+    projectId: "cmsc-4375-lab-3",
+    storageBucket: "cmsc-4375-lab-3.appspot.com",
+    messagingSenderId: "497802636802",
+    appId: "1:497802636802:web:6be554d28defa4158d411b",
+    measurementId: "G-FSX425BFDB"
+  }
 };
 
 /*
