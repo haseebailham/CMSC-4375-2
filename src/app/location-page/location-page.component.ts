ee2eded5b41451e945d76e6a1bfae78971aea677
b2333981 <b2333981@ben.edu>
2019-10-30 13:42:37 -0500

    Changed location component and service.ts file for getting locations from database

--- src/app/location-page/location-page.component.ts
+++ src/app/location-page/location-page.component.ts
@@ -12,13 +12,17 @@ export class LocationPageComponent implements OnInit {
   dataset = ['Bali, Indonesia', 'Dubai, UAE', 'Srinagar, India', 'Crete, Greece', 'Rome, Italy',
     'Copenhagen, Denmark', 'Interlaken, Switzerland', 'Miami, US', 'Cancun, Mexico',
     'Alberta, Canada', 'Anchorage, US'];
-  private locationList;
+  locationList;
+  cityList;
   searchText: string;
 
   constructor(public locationService: Service) { }
 
   ngOnInit() {
     this.locationList = this.locationService.getLocations().subscribe(res => (this.locationList = res));
+    for (let i = 0; i < this.locationList.length; i++) {
+      this.cityList[i] = this.locationList[i].payload.doc.data().City;
+      console.log(this.cityList.length);
+    }
   }
-
 }
